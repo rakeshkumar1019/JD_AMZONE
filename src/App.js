a87907d13d3ec9785ba8f02afa2a48bd2571293c
Rakesh Kumar  Singh <52422856+rakeshkumar1019@users.noreply.github.com>
2022-02-05 20:17:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Banner from './Components/Banner';
+import Nav from './Components/Nav';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import NotFound from './Components/NotFound';
+import Home from './Components/Home';
+import Footer from './Components/Footer';
 
 function App() {
   return (
+  <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Nav/>
+        <Routes>
+         <Route exec path="/" element={<Home />} />
+         <Route exec path="/banner" element={<Banner />} />
+         <Route exec path="*" element={<NotFound />} />
+        </Routes>
+        <Footer/>
     </div>
+  </BrowserRouter>
   );
 }
 
